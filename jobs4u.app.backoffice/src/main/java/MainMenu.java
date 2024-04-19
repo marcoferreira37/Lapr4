89ec5f01cebee04a9d5fed9f6d3b4e3105e44607
Francisco Silveira <1220813@isep.ipp.pt>
2024-04-19 15:45:33 +0100

    update backoffice

--- jobs4u.app.backoffice/src/main/java/MainMenu.java
+++ jobs4u.app.backoffice/src/main/java/MainMenu.java
@@ -1,130 +1,121 @@
-//import LogIn.CafeteriaUserBaseUI;
-//import eapli.framework.actions.Actions;
-//import eapli.framework.actions.menu.MenuItem;
-//import eapli.framework.actions.menu.Menu;
-//import eapli.framework.infrastructure.authz.application.AuthorizationService;
-//import eapli.framework.infrastructure.authz.application.AuthzRegistry;
-//import eapli.framework.infrastructure.authz.application.UserSession;
-//import eapli.framework.presentation.console.ExitWithMessageAction;
-//import eapli.framework.presentation.console.ListUI;
-//import eapli.framework.presentation.console.ShowMessageAction;
-//import eapli.framework.presentation.console.menu.MenuItemRenderer;
-//import eapli.framework.presentation.console.menu.VerticalMenuRenderer;
-//
-//import java.util.Optional;
-//
-//
-//class MainMenu extends CafeteriaUserBaseUI {
-//
-//    private static final String SEPARATOR_LABEL = "--------------";
-//
-//    private static final String RETURN = "Return ";
-//
-//    private static final String NOT_IMPLEMENTED_YET = "Not implemented yet";
-//
-//    private static final int EXIT_OPTION = 0;
-//
-//    // MAIN MENU
-//    private static final int MY_USER_OPTION = 1;
-//    private static final int BOOKINGS_OPTION = 2;
-//    private static final int ACCOUNT_OPTION = 3;
-//    private static final int SETTINGS_OPTION = 4;
-//
-//    // BOOKINGS MENU
-//    private static final int BOOK_A_MEAL_OPTION = 2;
-//    private static final int LIST_MY_BOOKINGS_OPTION = 3;
-//
-//    // ACCOUNT MENU
-//    private static final int LIST_MOVEMENTS_OPTION = 1;
-//
-//    // SETTINGS
-//    private static final int SET_USER_ALERT_LIMIT_OPTION = 1;
-//
-//    private final AuthorizationService authz = AuthzRegistry.authorizationService();
-//
-//    @Override
-//    public boolean show() {
-//        drawFormTitle();
-//        return doShow();
-//    }
-//
-//    /**
-//     * @return true if the user selected the exit option
-//     */
-//    @Override
-//    public boolean doShow() {
-//        final Menu menu = buildMainMenu();
-//        final var renderer = new VerticalMenuRenderer(menu, MenuItemRenderer.DEFAULT);
-//        return renderer.render();
-//    }
-//
-//    private Menu buildMainMenu() {
-//        final Menu mainMenu = new Menu();
-//
-//        final Menu myUserMenu = new MyUserMenu();
-//        mainMenu.addSubMenu(MY_USER_OPTION, myUserMenu);
-//
-//        mainMenu.addItem(MenuItem.separator(SEPARATOR_LABEL));
-//
-//        final Menu bookingsMenu = buildBookingsMenu();
-//        mainMenu.addSubMenu(BOOKINGS_OPTION, bookingsMenu);
-//
-//        mainMenu.addItem(MenuItem.separator(SEPARATOR_LABEL));
-//
-//        final Menu accountMenu = buildAccountMenu();
-//        mainMenu.addSubMenu(ACCOUNT_OPTION, accountMenu);
-//
-//        mainMenu.addItem(MenuItem.separator(SEPARATOR_LABEL));
-//
-//        final Menu settingsMenu = buildAdminSettingsMenu();
-//        mainMenu.addSubMenu(SETTINGS_OPTION, settingsMenu);
-//
-//        mainMenu.addItem(MenuItem.separator(SEPARATOR_LABEL));
-//
-//        mainMenu.addItem(EXIT_OPTION, "Exit", new ExitWithMessageAction("Bye, Bye"));
-//
-//        return mainMenu;
-//    }
-//
-//    private Menu buildAccountMenu() {
-//        final Menu accountMenu = new Menu("Account");
-//        accountMenu.addItem(LIST_MOVEMENTS_OPTION, "List movements", () -> {
-//            final Optional<UserSession> s = authz.session();
-//            // example of using an extended class of generic list ui from the
-//            // framework
-//            new ListMovementsUI<>(
-//                    () -> new ListMovementsController().allMovementsByUsername(
-//                            s.orElseThrow(IllegalStateException::new).authenticatedUser()
-//                                    .identity()),
-//                    new MovementPrinter(), "Movement", "List My Movements",
-//                    "No data").show();
-//            return true;
-//        });
-//        accountMenu.addItem(EXIT_OPTION, RETURN, Actions.SUCCESS);
-//        return accountMenu;
-//    }
-//
-//    private Menu buildBookingsMenu() {
-//        final Menu menu = new Menu("Bookings");
-//        menu.addItem(BOOK_A_MEAL_OPTION, "Book a meal", () -> new RegisterBookingUI().show());
-//
-//        menu.addItem(LIST_MY_BOOKINGS_OPTION, "My Bookings", () -> {
-//            new ListUI<>(new MyBookingsController().myBookings(), new BookingPrinter(), "Booking",
-//                    String.format("   %-40s %-15s %s", "TOKEN", "DAY", "STATUS"), "List My Bookings",
-//                    "No data").show();
-//            return true;
-//        });
-//        menu.addItem(EXIT_OPTION, RETURN, Actions.SUCCESS);
-//        return menu;
-//    }
-//
-//    private Menu buildAdminSettingsMenu() {
-//        final Menu menu = new Menu("Settings >");
-//
-//        menu.addItem(SET_USER_ALERT_LIMIT_OPTION, "Set users' alert limit",
-//                new ShowMessageAction(NOT_IMPLEMENTED_YET));
-//        menu.addItem(EXIT_OPTION, RETURN, Actions.SUCCESS);
-//
-//        return menu;
-//    }
-//}
+
+import eapli.framework.actions.Actions;
+import eapli.framework.actions.menu.Menu;
+import eapli.framework.actions.menu.MenuItem;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import eapli.framework.presentation.console.AbstractUI;
+import eapli.framework.presentation.console.ExitWithMessageAction;
+import eapli.framework.presentation.console.ShowMessageAction;
+import eapli.framework.presentation.console.menu.HorizontalMenuRenderer;
+import eapli.framework.presentation.console.menu.MenuItemRenderer;
+import eapli.framework.presentation.console.menu.MenuRenderer;
+import eapli.framework.presentation.console.menu.VerticalMenuRenderer;
+
+/**
+ * TODO split this class in more specialized classes for each menu
+ *
+ * @author Paulo Gandra Sousa
+ */
+public class MainMenu extends AbstractUI {
+
+    private static final String RETURN_LABEL = "Return ";
+
+    private static final int EXIT_OPTION = 0;
+
+    // USERS
+    private static final int ADD_USER_OPTION = 1;
+    private static final int LIST_USERS_OPTION = 2;
+    private static final int DEACTIVATE_USER_OPTION = 3;
+    private static final int ACCEPT_REFUSE_SIGNUP_REQUEST_OPTION = 4;
+
+    // SETTINGS
+    private static final int SET_KITCHEN_ALERT_LIMIT_OPTION = 1;
+
+    // MAIN MENU
+    private static final int MY_USER_OPTION = 1;
+    private static final int USERS_OPTION = 2;
+    private static final int SETTINGS_OPTION = 4;
+
+    private static final String SEPARATOR_LABEL = "--------------";
+
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
+
+    @Override
+    public boolean show() {
+        drawFormTitle();
+        return doShow();
+    }
+
+    /**
+     * @return true if the user selected the exit option
+     */
+    @Override
+    public boolean doShow() {
+        final Menu menu = buildMainMenu();
+        final MenuRenderer renderer;
+        if (Application.settings().isMenuLayoutHorizontal()) {
+            renderer = new HorizontalMenuRenderer(menu, MenuItemRenderer.DEFAULT);
+        } else {
+            renderer = new VerticalMenuRenderer(menu, MenuItemRenderer.DEFAULT);
+        }
+        return renderer.render();
+    }
+
+    @Override
+    public String headline() {
+
+        return authz.session().map(s -> "Base [ @" + s.authenticatedUser().identity() + " ]")
+                .orElse("Base [ ==Anonymous== ]");
+    }
+
+    private Menu buildMainMenu() {
+        final Menu mainMenu = new Menu();
+
+        final Menu myUserMenu = new MyUserMenu();
+        mainMenu.addSubMenu(MY_USER_OPTION, myUserMenu);
+
+        if (!Application.settings().isMenuLayoutHorizontal()) {
+            mainMenu.addItem(MenuItem.separator(SEPARATOR_LABEL));
+        }
+
+        if (authz.isAuthenticatedUserAuthorizedTo(BaseRoles.POWER_USER, BaseRoles.ADMIN)) {
+            final Menu usersMenu = buildUsersMenu();
+            mainMenu.addSubMenu(USERS_OPTION, usersMenu);
+            final Menu settingsMenu = buildAdminSettingsMenu();
+            mainMenu.addSubMenu(SETTINGS_OPTION, settingsMenu);
+        }
+
+        if (!Application.settings().isMenuLayoutHorizontal()) {
+            mainMenu.addItem(MenuItem.separator(SEPARATOR_LABEL));
+        }
+
+        mainMenu.addItem(EXIT_OPTION, "Exit", new ExitWithMessageAction("Bye, Bye"));
+
+        return mainMenu;
+    }
+
+    private Menu buildAdminSettingsMenu() {
+        final Menu menu = new Menu("Settings >");
+
+        menu.addItem(SET_KITCHEN_ALERT_LIMIT_OPTION, "Set kitchen alert limit",
+                new ShowMessageAction("Not implemented yet"));
+        menu.addItem(EXIT_OPTION, RETURN_LABEL, Actions.SUCCESS);
+
+        return menu;
+    }
+
+    private Menu buildUsersMenu() {
+        final Menu menu = new Menu("Users >");
+
+        menu.addItem(ADD_USER_OPTION, "Add User", new AddUserUI()::show);
+        menu.addItem(LIST_USERS_OPTION, "List all Users", new ListUsersAction());
+        menu.addItem(DEACTIVATE_USER_OPTION, "Deactivate User", new DeactivateUserAction());
+        menu.addItem(ACCEPT_REFUSE_SIGNUP_REQUEST_OPTION, "Accept/Refuse Signup Request",
+                new AcceptRefuseSignupRequestAction());
+        menu.addItem(EXIT_OPTION, RETURN_LABEL, Actions.SUCCESS);
+
+        return menu;
+    }
+
+}
