905a09c8ef1528ee7c64c74e1e17df4279bdcd56
Marco <1220913@isep.ipp.pt>
2024-05-13 15:18:45 +0100

    #24 - Added Fiscos Copyright

--- jobs4u.infrastructure/src/main/java/eapli/base/Application.java
+++ jobs4u.infrastructure/src/main/java/eapli/base/Application.java
@@ -28,8 +28,8 @@ package eapli.base;
  */
 public class Application {
 
-    public static final String VERSION = "1.4.0";
-    public static final String COPYRIGHT = "(C) 2016 - 2021, ISEP's Professors of EAPLI";
+    public static final String VERSION = "1.0.0";
+    public static final String COPYRIGHT = "(C) 2023 - 2024, FISCOS - All Rights Reserved";
 
     private static final AppSettings SETTINGS = new AppSettings();
 
