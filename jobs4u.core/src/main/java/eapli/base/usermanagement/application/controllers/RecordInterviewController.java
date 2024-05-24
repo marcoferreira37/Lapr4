6b863eac58345c8a72671177680ed80ba4111e49
Francisco Silveira <1220813@isep.ipp.pt>
2024-05-24 15:58:12 +0100

    #39 fixed bugs in UI and Controller

--- jobs4u.core/src/main/java/eapli/base/usermanagement/application/controllers/RecordInterviewController.java
+++ jobs4u.core/src/main/java/eapli/base/usermanagement/application/controllers/RecordInterviewController.java
@@ -1,9 +1,11 @@
 package eapli.base.usermanagement.application.controllers;
 
+import eapli.base.candidate.CandidateManagementService;
 import eapli.base.domain.jobApplication.JobOpeningApplication;
 import eapli.base.domain.candidate.Candidate;
 import eapli.base.infrastructure.persistence.PersistenceContext;
 import eapli.base.repositories.JobOpeningApplicationRepository;
+import eapli.base.service.JobOpeningApplicationService;
 
 import java.util.List;
 
@@ -12,11 +14,23 @@ public class RecordInterviewController {
 
     private final JobOpeningApplicationRepository repo = PersistenceContext.repositories().jobApplications();
 
-    public List<JobOpeningApplication> getJobOpeningApplication() {
-        return (List<JobOpeningApplication>) repo.findAll();
+    private final JobOpeningApplicationService service = new JobOpeningApplicationService(repo);
+
+    private final CandidateManagementService candidateManagementService = new CandidateManagementService();
+
+//    public List<JobOpeningApplication> getJobOpeningApplication() {
+//        return (List<JobOpeningApplication>) repo.findAll();
+//    }
+
+    public Iterable<JobOpeningApplication> getJobOpeningApplication(){
+    return service.allJobOpeningApplication();
     }
 
-    public Candidate getCandidate(JobOpeningApplication jobOpeningApplication) {
-        return jobOpeningApplication.candidate();
+//    public Candidate getCandidate(JobOpeningApplication jobOpeningApplication) {
+//        return jobOpeningApplication.candidate();
+//    }
+
+    public Iterable<Candidate> getCandidate(JobOpeningApplication jobOpeningApplication) {
+        return candidateManagementService.getCandidate(jobOpeningApplication);
     }
 }
