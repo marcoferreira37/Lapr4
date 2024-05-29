060f72f375d88dcc33fa6c57d3aa92be7ccc20b4
DESKTOP-HH6R44T\Utilizador <1221330@isep.ipp.pt>
2024-05-29 17:11:30 +0100

    #37 adding authentication service, now only the operator and the admin can enable or disable a candidate. 2000b

--- jobs4u.core/src/main/java/eapli/base/usermanagement/application/controllers/DisableCandidateController.java
+++ jobs4u.core/src/main/java/eapli/base/usermanagement/application/controllers/DisableCandidateController.java
@@ -4,6 +4,9 @@ import eapli.base.domain.Jobs4UUser;
 import eapli.base.clientusermanagement.repositories.Ijobs4UUserRepository;
 import eapli.base.domain.candidate.Candidate;
 import eapli.base.repositories.CandidateRepository;
+import eapli.base.usermanagement.domain.BaseRoles;
+import eapli.framework.infrastructure.authz.application.AuthorizationService;
+import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 import eapli.framework.infrastructure.authz.domain.model.SystemUser;
 import eapli.framework.infrastructure.authz.domain.repositories.UserRepository;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,6 +17,8 @@ import java.util.Calendar;
 public class DisableCandidateController {
 
     private final CandidateRepository candidateRepository;
+
+    private final AuthorizationService authz = AuthzRegistry.authorizationService();
 
     /**
      * Instantiates a controller for disabling a user.
@@ -36,6 +41,7 @@ public class DisableCandidateController {
      */
     @Transactional
     public Candidate disableCandidate(String email) {
+        authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.OPERATOR, BaseRoles.ADMIN);
         for (Candidate user : candidateRepository.findAll()){
             if (user.emailAddress().toString().equals(email)) {
                 user.user().deactivate(Calendar.getInstance());
