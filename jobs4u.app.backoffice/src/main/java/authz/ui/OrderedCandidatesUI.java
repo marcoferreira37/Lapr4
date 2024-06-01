286bc1077a31da14a5eb2ad5bee6d6e100b731af
Gonçalo Sousa <1221331@isep.ipp.pt>
2024-06-01 11:35:24 +0100

    #44 - [US1019] Update of the class OrderedCandidatesUI

--- jobs4u.app.backoffice/src/main/java/authz/ui/OrderedCandidatesUI.java
+++ jobs4u.app.backoffice/src/main/java/authz/ui/OrderedCandidatesUI.java
@@ -1,8 +1,70 @@
 package authz.ui;
 
-public class OrderedCandidatesUI implements {
+import eapli.base.app.common.console.ui.components.AbstractUI;
+import eapli.base.app.common.console.ui.components.ColorCode;
+import eapli.base.app.common.console.ui.components.Console;
+import eapli.base.domain.jobApplication.JobOpeningApplication;
+import eapli.base.domain.jobOpening.JobOpening;
+import eapli.base.usermanagement.application.controllers.ListAllApplicationsForJobOpeningController;
+
+import java.util.List;
+
+public class OrderedCandidatesUI extends AbstractUI {
+
+    private final ListAllApplicationsForJobOpeningController controllerApplication = new ListAllApplicationsForJobOpeningController();
+
+    @Override
+    protected boolean doShow() {
+
+        System.out.println("=====================================================");
+        System.out.println("|| List of Job Openings ||");
+        List<JobOpening> openingList = (List<JobOpening>) controllerApplication.allJobOpenings();
+        if (openingList.isEmpty()) {
+            System.out.println("No job openings available!");
+            return false;
+        }
+        printNumeratedList(openingList);
+        int option = getUserOption(openingList.size());
+        JobOpening jobOpening = openingList.get(option - 1);
+        List<JobOpeningApplication> applications = controllerApplication.allApplicationsForJobOpening(jobOpening);
+        if (applications.isEmpty()) {
+            System.out.println("There are no applications for this job opening!");
+            return false;
+        }
+
+        return false;
+    }
+
+    @Override
+    public String headline() {
+        return "Ordered Candidates";
+    }
+
+    public void printNumeratedList(List<JobOpening> openingList) {
+        System.out.printf("%s\n\n", "\nChoose a job opening: ");
+
+        int index = 1;
+        for (JobOpening item : openingList) {
+            System.out.println(ColorCode.BLUE.getValue() + index + ColorCode.RESET.getValue() + " - " + "Job Reference: " + item.identity().fullReference());
+            index++;
+        }
+        System.out.println();
+    }
+
+    private int getUserOption(int size) {
+        int option;
+        while (true) {
+            try {
+                option = Integer.parseInt(Console.readLine("Select a job opening: "));
+                if (option > 0 && option <= size) {
+                    break;
+                } else {
+                    System.out.println("Invalid option. Please choose a number between 1 and " + size + ".");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please enter a valid number.");
+            }
+        }
+        return option;
+    }
 }
-
-
-
-//O candidato pede para ver applications
