752ccd569b44e71443eb8a03e0e694a82a5659df
Francisco Silveira <1220813@isep.ipp.pt>
2024-06-02 19:23:24 +0100

    #39 update JobInterviewTest

---
+++ jobs4u.core/src/test/java/eapli/base/domain/jobOpeningInterview/JobInterviewTest.java
@@ -0,0 +1,48 @@
+package eapli.base.domain.jobOpeningInterview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JobInterviewTest {
+
+    @Test
+    void jobInterview() {
+    }
+
+    @Test
+    void sameAs() {
+    }
+
+    @Test
+    void identity() {
+    }
+
+    @Test
+    void jobOpeningApplication() {
+    }
+
+    @Test
+    void interviewDate() {
+    }
+
+    @Test
+    void interviewTime() {
+    }
+
+    @Test
+    void grade() {
+    }
+
+    @Test
+    void testToString() {
+    }
+
+    @Test
+    void gradeInterview() {
+    }
+
+    @Test
+    void getInterviewTime() {
+    }
+}
