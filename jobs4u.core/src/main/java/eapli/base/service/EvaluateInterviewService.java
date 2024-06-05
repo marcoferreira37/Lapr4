4122e6d0dbd1c210c2f3eb4c54fbe85463fa1bce
Francisco Silveira <1220813@isep.ipp.pt>
2024-06-05 17:56:41 +0100

    #43 add Service

---
+++ jobs4u.core/src/main/java/eapli/base/service/EvaluateInterviewService.java
@@ -0,0 +1,20 @@
+package eapli.base.service;
+
+import plugin.InterviewModel;
+
+import java.io.IOException;
+
+public class EvaluateInterviewService {
+    public void evaluateInterview(String file) throws IOException {
+
+        InterviewModel interviewModel = new InterviewModel();
+
+
+        interviewModel.enterStart(file);
+
+
+    }
+
+
+
+}
