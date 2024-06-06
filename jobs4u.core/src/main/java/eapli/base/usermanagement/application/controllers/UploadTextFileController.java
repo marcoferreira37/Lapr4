da5e7b06ec3796a48a97f48a945da32099f532a9
DESKTOP-HH6R44T\Utilizador <1221330@isep.ipp.pt>
2024-06-06 19:22:36 +0100

    #48 updating the controller for 2004, now fully functional

--- jobs4u.core/src/main/java/eapli/base/usermanagement/application/controllers/UploadTextFileController.java
+++ jobs4u.core/src/main/java/eapli/base/usermanagement/application/controllers/UploadTextFileController.java
@@ -1,6 +1,8 @@
 package eapli.base.usermanagement.application.controllers;
 
 import eapli.base.candidate.CandidateManagementService;
+import eapli.base.domain.PlugIn.JobRequirements.gen.JobRequirementsGrammarLexer;
+import eapli.base.domain.PlugIn.JobRequirements.gen.JobRequirementsGrammarParser;
 import eapli.base.domain.candidate.Candidate;
 import eapli.base.domain.jobApplication.JobOpeningApplication;
 import eapli.base.infrastructure.persistence.PersistenceContext;
@@ -8,6 +10,10 @@ import eapli.base.repositories.JobOpeningApplicationRepository;
 import eapli.base.usermanagement.domain.BaseRoles;
 import eapli.framework.infrastructure.authz.application.AuthorizationService;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 
 import java.io.File;
 import java.io.IOException;
@@ -31,20 +37,47 @@ public class UploadTextFileController {
         return candidateSvc.allCandidate();
     }
 
-    public void uploadFile(JobOpeningApplication app, String path) {
+    public void uploadFile(JobOpeningApplication app, String file) {
         authz.ensureAuthenticatedUserHasAnyOf(BaseRoles.OPERATOR, BaseRoles.ADMIN, BaseRoles.POWER_USER);
 
         try {
-            Path source = Paths.get(path);
-            Path destinatinPAth = Paths.get("jobs4u.core/src/main/resources/PlugIns/RequirementsSpecifications/backEndDeveloperRequirements");
-            Path destination =  destinatinPAth.resolve( app.jobOpening().getJobReference().fullReference() + ".txt");
-            Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
-            String fileName = app.jobOpening().getJobReference().fullReference() + ".txt";
-            app.setCandidateRequirements(fileName);
+            if(passGrammar(file)){
+                Path source = Paths.get(file);
+                Path destinatinPAth = Paths.get("jobs4u.core/src/main/resources/PlugIns/RequirementsSpecifications/backEndDeveloperRequirements");
+                Path destination =  destinatinPAth.resolve( app.jobOpening().getJobReference().fullReference() + ".txt");
+                Files.copy(source, destination, StandardCopyOption.REPLACE_EXISTING);
+                String fileName = app.jobOpening().getJobReference().fullReference() + ".txt";
+                app.setCandidateRequirements(fileName);
+            } else{
+                System.out.println("Error: File didnt match grammar");
+            }
         } catch (IOException e) {
             System.err.println("Error generating file" + e.getMessage());
         }
         applicationRepo.save(app);
+    }
+
+    public boolean passGrammar(String path) {
+        if(path == null){
+            System.out.println("File not found");
+            return false;
+        }
+
+        try{
+            CharStream input = CharStreams.fromFileName(path);
+            JobRequirementsGrammarLexer lexer = new JobRequirementsGrammarLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            JobRequirementsGrammarParser parser = new JobRequirementsGrammarParser(tokens);
+            ParseTree tree = parser.start();
+
+        } catch (IOException e) {
+            System.err.println("Error reading file: " + e.getMessage());
+        } catch (Exception e){
+            System.err.println("Error verifying grammar: " + e.getMessage());
+        }
+
+        return true;
+
     }
 
     public List<JobOpeningApplication> getApplicationsByCandidate(Candidate c) {
