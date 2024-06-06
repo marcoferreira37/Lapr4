9ec3ad8552b780a5a44553e23b045677538d05c3
Marco <1220913@isep.ipp.pt>
2024-06-06 19:17:28 +0100

    #40 [1015] Implemented basic method to verify Grammar

--- jobs4u.core/src/main/java/eapli/base/domain/PlugIn/JobRequirements/JobRequirements.java
+++ jobs4u.core/src/main/java/eapli/base/domain/PlugIn/JobRequirements/JobRequirements.java
@@ -10,74 +10,39 @@ import org.antlr.v4.runtime.tree.ParseTree;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Map;
 
-public class JobRequirements implements JobRequirementInterface {
-    @Override
-    public File generateTxt(String path) {
-        // Create a File object for the desired path
-        File file = new File(path);
+public class JobRequirements {
 
-        try {
-            FileWriter writer = new FileWriter(path);
-
-            // Write the prompts
-            writer.write("----- JobRequirement2 -----\n\n\n");
-
-            writer.write("Requirement #1: Years of experience\n");
-            writer.write("Integer:\n\n");
-
-            writer.write("Requirement #2: Know how to operate in a big kitchen\n");
-            writer.write("TrueOrFalse:\n\n");
-
-            writer.close();
-
-        } catch (IOException e) {
-            System.err.println("Error writing to file: " + e.getMessage());
-        }
-
-        return file;
-    }
-
-    @Override
-    public String validate(String path) throws IOException {
-        //verify if the file exists
-        File file = new File(path);
-        if (!file.exists()) {
-            throw new IOException("File not found");
-        }
-
-        try {
-            JobRequirementsGrammarLexer lexer = new JobRequirementsGrammarLexer(CharStreams.fromFileName(path));
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            JobRequirementsGrammarParser parser = new JobRequirementsGrammarParser(tokens);
-            ParseTree tree = parser.start();
-            String validation = parseWithVisitor(tree);
-            return validation;
-        } catch (Exception e) {
-            System.err.println("Error parsing file: " + e.getMessage());
-            return null;
-        }
-    }
-
-    private static String parseWithVisitor(ParseTree tree) {
+    private Map<String, String> parseWithVisitor(ParseTree tree) {
         RequirementsValidator validator = new RequirementsValidator();
-        validator.visit(tree);
-
-        return validator.getValidation();
+        return validator.visit(tree);
     }
 
     public boolean verifyRequirements(String  candidateRequirements, String jobRequirements) {
         try {
-            String validation = validate(candidateRequirements);
-           if (!validation.equals("Success")) {
-               System.err.println("Error validating requirements: " + validation);
-           }
-           validation= validate(jobRequirements);
-              if (!validation.equals("Success")) {
-                System.err.println("Error validating requirements: " + validation);
-              }
-        } catch (IOException e) {
-            System.err.println("Error validating requirements: " + e.getMessage());
+            JobRequirementsGrammarLexer requirementsGrammarLexer = new JobRequirementsGrammarLexer(CharStreams.fromString(candidateRequirements));
+            CommonTokenStream tokens = new CommonTokenStream(requirementsGrammarLexer);
+            JobRequirementsGrammarParser parser = new JobRequirementsGrammarParser(tokens);
+            ParseTree tree = parser.start();
+            Map<String, String> candidateRequirementsMap = parseWithVisitor(tree);
+
+
+            JobRequirementsGrammarLexer jobRequirementsGrammarLexer = new JobRequirementsGrammarLexer(CharStreams.fromString(jobRequirements));
+            CommonTokenStream tokens2 = new CommonTokenStream(jobRequirementsGrammarLexer);
+            JobRequirementsGrammarParser parser2 = new JobRequirementsGrammarParser(tokens2);
+            ParseTree tree2 = parser2.start();
+            Map<String, String> jobRequirementsMap = parseWithVisitor(tree2);
+
+            for (Map.Entry<String, String> entry : jobRequirementsMap.entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                if (!candidateRequirementsMap.containsKey(key) || !candidateRequirementsMap.get(key).equals(value)) {
+                    return false;
+                }
+            }
+        } catch (Exception e) {
+            System.err.println("Error parsing file: " + e.getMessage());
             return false;
         }
         return true;
