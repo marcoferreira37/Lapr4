b160cce5f81f2832f00eeb59f9a0052369915e23
DESKTOP-HH6R44T\Utilizador <1221330@isep.ipp.pt>
2024-06-08 19:41:02 +0100

    #50 BaseCandidateApp for 3001 done

--- jobs4u.app.candidate/src/main/java/app/candidate/console/BaseCandidateApp.java
+++ jobs4u.app.candidate/src/main/java/app/candidate/console/BaseCandidateApp.java
@@ -21,34 +21,95 @@
 package app.candidate.console;
 
 import app.candidate.console.presentation.FrontMenu;
+
 import eapli.base.infrastructure.persistence.PersistenceContext;
+import eapli.base.protocol.ComCodes;
+import eapli.base.protocol.Notifications;
+import eapli.base.protocol.V0Protocol;
+import eapli.base.protocol.dto.LoginDTO;
 import eapli.base.usermanagement.domain.BasePasswordPolicy;
+import eapli.base.usermanagement.domain.BaseRoles;
 import eapli.framework.infrastructure.authz.application.AuthzRegistry;
 import eapli.framework.infrastructure.authz.domain.model.PlainTextEncoder;
+import eapli.framework.infrastructure.authz.domain.model.Role;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  * Base User App.
  */
 @SuppressWarnings("squid:S106")
 public final class BaseCandidateApp {
-
+    static Socket socket;
+static V0Protocol protocol;
     /**
      * Empty constructor is private to avoid instantiation of this class.
      */
     private BaseCandidateApp() {
     }
 
-    public static void main(final String[] args) {
+    public static void main(final String[] args) throws IOException, ClassNotFoundException {
         System.out.println("=====================================");
         System.out.println("Base Candidate App");
         System.out.println("(C) 2016 - 2019");
+        System.out.println("Fiscos Imortalized!");
         System.out.println("=====================================");
 
-        AuthzRegistry.configure(PersistenceContext.repositories().users(),
-                new BasePasswordPolicy(), new PlainTextEncoder());
+        //AuthzRegistry.configure(PersistenceContext.repositories().users(),
+          //      new BasePasswordPolicy(), new PlainTextEncoder());
 
-        new FrontMenu().show();
+       // new FrontMenu().show();
         // exiting the application, closing all threads
+
+        List<Notifications> notificationsList;
+        int option;
+        boolean flag;
+        do{
+            socket= new Socket("127.0.0.1", 21782);
+            protocol = new V0Protocol(socket);
+            try{
+                System.out.println("What is your username?");
+                Scanner scanner = new Scanner(System.in);
+                String userName = scanner.nextLine();
+                System.out.println("What is your password?");
+                String password= scanner.nextLine();
+                protocol.send(ComCodes.AUTH.getValue(), new LoginDTO(userName, password, BaseRoles.CANDIDATE));
+                protocol.receive(ComCodes.ACK.getValue());
+                flag=true;
+            }catch(Exception e){
+                System.out.println("Invalid username or password. Try again.");
+                flag=false;
+            }
+            if(flag){
+                protocol.send(ComCodes.NOTIF.getValue(), "");
+                notificationsList = protocol.receive(ComCodes.NOTIF.getValue());
+                if(!notificationsList.isEmpty()){
+                    for(Notifications notification: notificationsList){
+                        System.out.println(notification.toString());
+                    }
+                }
+                do{
+                    System.out.println("1- List Applications");
+                    System.out.println("2- Exit");
+                    Scanner scanner = new Scanner(System.in);
+                    option = scanner.nextInt();
+                    switch(option){
+                        case 1:
+                            break;
+                        case 2:
+                            protocol.send(ComCodes.DISCON.getValue(), null);
+                            break;
+                        default:
+                            System.out.println("Invalid option. Try again.");
+                    }
+                }while(option!=2);
+            }
+        }while(!flag);
+
+
         System.exit(0);
 
     }
