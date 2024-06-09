9cb565eea8a42b4083b75916f791785c8ef9131a
Francisco Silveira <1220813@isep.ipp.pt>
2024-06-09 19:58:05 +0100

    update

--- jobs4u.core/src/main/java/eapli/base/domain/EmailService/SendEmails.java
+++ jobs4u.core/src/main/java/eapli/base/domain/EmailService/SendEmails.java
@@ -6,6 +6,7 @@ import eapli.base.domain.jobOpening.JobOpening;
 import eapli.base.usermanagement.application.EmailManagement.EmailMessageGenerator;
 import eapli.base.usermanagement.application.services.ApplicationService;
 import eapli.base.usermanagement.application.services.JobOpeningService;
+import eapli.framework.general.domain.model.EmailAddress;
 import eapli.framework.infrastructure.authz.domain.model.SystemUser;
 
 import javax.mail.*;
@@ -17,12 +18,10 @@ import java.util.Properties;
 public class SendEmails {
 
     private Session session;
-    private final JobOpeningService jobOpeningService = new JobOpeningService();
-    private final ApplicationService applicationService = new ApplicationService();
-    private final EmailService emailService = new EmailService();
 
 
-    public void sendEmail() {
+
+    public void sendEmail(EmailAddress toUser, char[] content) {
 
 
         final String username = "grupoxlapr@outlook.pt";
@@ -41,79 +40,28 @@ public class SendEmails {
                     }
                 });
 
+        email(toUser,content);
 
-        Iterable<EmailToSend> emailToSends = emailService.findToSends();
-        List<JobOpening> jobOpenings = getJobOpenings();
-        JobOpening selectedJobOpening;
-        for (JobOpening job : jobOpenings) {
-            for (EmailToSend emails : emailToSends) {
-                if (emails.email().equals(job.getCompany().getCustomer().user().email())) {
-                    selectedJobOpening = job;
-                    List<Candidate> candidates = getRankedApplications(selectedJobOpening);
-                    publishResults(candidates, selectedJobOpening, emailToSends);
-                }
-
-            }
-        }
     }
 
-
-    public List<Candidate> getRankedApplications(JobOpening selectedJobOpening) {
-        return applicationService.getRankedApplications(selectedJobOpening);
-    }
-
-    public List<JobOpening> getJobOpenings() {
-        return jobOpeningService.allJobs();
-    }
-
-
-    public void sendEmailToCandidate(Candidate toUser, String content) {
+    public void email(EmailAddress toUser, char[] content){
 
         Thread emailThread = new Thread(() -> {
             try {
-                System.out.println("Sending email to " + toUser.user().email().toString());
+                System.out.println("Sending email to " + toUser.toString());
                 Message message = new MimeMessage(session);
                 message.setFrom(new InternetAddress("grupoxlapr@outlook.pt"));
                 message.setRecipients(
                         Message.RecipientType.TO,
-                        InternetAddress.parse(toUser.user().email().toString())
+                        InternetAddress.parse(toUser.toString())
                 );
 
-                EmailMessageGenerator emailMessageGenerator = new EmailMessageGenerator();
-
-                message.setSubject(emailMessageGenerator.getACCEPTED_APPLICATION_SUBJECT());
-                message.setText(content);
-
-                Transport.send(message);
-
-            } catch (MessagingException e) {
-                System.err.println("Failed to send email: " + e.getMessage());
-            } finally {
-                System.out.println("Email thread finished");
-            }
-        });
-
-        emailThread.start();
-    }
-
-    public void sendEmailToClient(SystemUser toUser, String content) {
-        Thread emailThread = new Thread(() -> {
-            try {
-                System.out.println("Sending email to: " + toUser.email().toString());
-                Message message = new MimeMessage(session);
-                message.setFrom(new InternetAddress("grupoxlapr@outlook.pt"));
-                message.setRecipients(
-                        Message.RecipientType.TO,
-                        InternetAddress.parse(toUser.email().toString())
-                );
-
-                EmailMessageGenerator emailMessageGenerator = new EmailMessageGenerator();
-
-                message.setSubject(emailMessageGenerator.getJOB_CANDIDATES_LIST_SUBJECT());
-                message.setText(content);
+                message.setSubject("Jobs4U applications:");
+                String s = clean(content);
+                message.setText(s);
 
                 Transport.send(message);
-                System.out.println("Email Sent to Client: " + toUser.email().toString());
+                System.out.println("Email Sent to: " + toUser.toString());
 
             } catch (MessagingException e) {
                 System.err.println("Failed to send email: " + e.getMessage());
@@ -121,29 +69,6 @@ public class SendEmails {
         });
 
         emailThread.start();
-    }
-
-    public void publishResults(List<Candidate> candidates, JobOpening jobOpening, Iterable<EmailToSend> emails) {
-        try {
-            for (EmailToSend email : emails) {
-                for (Candidate candidate : candidates) {
-                    if (email.email().equals(candidate.getEmail())) {
-                        String content = clean( email.content() );
-                        sendEmailToCandidate(candidate, content);
-                        emailService.setEmailAsSent(email);
-                    }
-                }
-                SystemUser systemUser = jobOpening.getCompany().findCustomer().user();
-                if (email.email().equals(systemUser.email())) {
-                    String content =  clean( email.content() );
-                    sendEmailToClient(systemUser, content);
-                    emailService.setEmailAsSent(email);
-                }
-            }
-
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
 
     }
 
@@ -154,4 +79,5 @@ public class SendEmails {
         }
         return result.toString();
     }
+
 }
